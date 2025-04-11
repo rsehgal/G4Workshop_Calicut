dbee6a97122f45a63271c028c7cf5fa006b643ca
Raman Sehgal <sc.ramansehgal@gmail.com>
2025-04-12 04:56:23 +0530

    Added VERBOSE guard

--- MuonDecay/src/MuonDecay_SteppingAction.cpp
+++ MuonDecay/src/MuonDecay_SteppingAction.cpp
@@ -20,20 +20,26 @@ void MuonDecay_SteppingAction::UserSteppingAction(const G4Step *step)
     if (track->GetParentID() == 0 && !MuonDecay_EventAction::muonStopped) {
       if (track->GetKineticEnergy() < 1 * eV) {
         MuonDecay_EventAction::muonStopped = true;
+	#ifdef VERBOSE
         std::cout << "Particle : " << track->GetDefinition()->GetParticleName()
                   << " : TrackID : " << track->GetTrackID() << " : ParentID : " << track->GetParentID()
                   << " : Energy : " << (track->GetKineticEnergy() / MeV)
                   << " : Time : " << (track->GetGlobalTime() / ns) << " : Creator : RAMAN" << std::endl;
+	#endif
       }
     } else if (!MuonDecay_EventAction::muonDecayed && track->GetParentID()>0) {
+      //If the muon is stopped then detecting the decay time, by detecting the creator process is "Decay"
+      //and the decayed product is e+ or e-
       if (track->GetCreatorProcess()->GetProcessName() == "Decay" &&
           (track->GetDefinition()->GetParticleName() == "e+" || track->GetDefinition()->GetParticleName() == "e-")) {
 	MuonDecay_EventAction::muonDecayed = true;
+        #ifdef VERBOSE
         std::cout << "Particle : " << track->GetDefinition()->GetParticleName()
                   << " : TrackID : " << track->GetTrackID() << " : ParentID : " << track->GetParentID()
                   << " : Energy : " << (track->GetKineticEnergy() / MeV)
                   << " : Time : " << (track->GetGlobalTime() / ns)
                   << " : Creator : " << track->GetCreatorProcess()->GetProcessName() << std::endl;
+ 	#endif
       }
     }
   }
